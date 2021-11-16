624a071117e23f577ab2ddea680cd9bf094dc3f4
Abigail <abigailviolet6@gmail.com>
2021-11-16 17:52:07 +0000

    week 12 lecture notes

---
+++ Week-12/index.js
@@ -0,0 +1,3 @@
+let p2 = document.getElementById("subtitle1");
+p2.style.backgroundColor = "red";
+document.querySelector('#subtitle1').innderHTML += "this is not a prank";
---
+++ Week-12/wk-12-actually/index.js
@@ -0,0 +1,24 @@
+console.log('hello!');
+
+let pics = 8;
+let pic;
+
+for(i=0;i<pics;i++){
+  pic = 1+i;
+document.getElementById('portfolio').innerHTML += "<div class = 'pimage'><img src='assets/portfolio/" + pic + ".jpg'" + "class='portfoliopic' id='pic" + pic+ "' />";
+}
+
+let displaydetails = false;
+document.getElementById('portfolio').addEventListener("click", showDetails);
+
+function showDetails(){
+  document.getElementById('details').style.display="black";
+  document.getElementById('detailsoverlay').style.display = "black";
+  document.getElementById('detailsoverlay').addEventListener('click', clickedOverlay);
+  displaydetails = true;
+}
+function clickedOverlay(){
+  document.getElementById('details').style.display="none";
+  document.getElementById('detailsoverlay').style.display = "none";
+  displaydetails = false;
+}
